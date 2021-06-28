ab9250007fdf6c972f1971de6e734ca3b1a11d68
Toby Tang <tobykuxn@icloud.com>
2021-06-28 22:29:12 +0800

    15. Understanding Classes

---
+++ section-2/15/index.js
@@ -0,0 +1,26 @@
+class Human {
+  constructor() {
+    this.gender = "Female";
+  }
+
+  printGender() {
+    console.log(this.gender);
+  }
+}
+
+class Person extends Human {
+  constructor() {
+    super();
+    this.name = "Toby";
+    this.gender = "Male";
+  }
+
+  printMyName() {
+    console.log(this.name);
+  }
+}
+
+const person = new Person();
+
+person.printMyName();
+person.printGender();
